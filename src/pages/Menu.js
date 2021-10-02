4e7f77218442daa829dddf101508abf736a6ddd4
Lacrima98654 <romeokakpo3@gmail.com>
2021-10-02 23:38:41 +0100

    ok

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -4,9 +4,15 @@ import {Link} from 'react-router-dom'
 class Menu extends React.Component{
     render() {
         return (
-            <div>
-                <Link to='/space_coding_lovers/quiz'>Quiz</Link><br/>
-                <Link to='/space_coding_lovers/puzzle'>Puzzle</Link>
+            <div className='container d-flex flex-column justify-content-center menu align-items-between menu'>
+                <button className='btn'>
+                    <Link to='/space_coding_lovers/quiz'>Quiz</Link>
+                </button>
+                <button className='btn btn-primary'>
+                    <Link to='/space_coding_lovers/puzzle'>Puzzle</Link>
+                </button>
+               
+               
             </div>
         )
     }
